7aa889cab3049101b246ee4b861526d4446ae0ea
XDCoolMan <dcorniello.business@gmail.com>
2023-10-30 16:33:31 -0400

    Reworked parser
    Added a user class that has a method of parsing the inputs, this way we can hold more information about the user at that moment.

--- processing.py
+++ processing.py
@@ -1,5 +1,8 @@
 from datetime import datetime as dt
 
+# Debug flag for testing.
+debug_flag = True
+
 # Class: date and time
 # Purpose: hold and format a date and time structure
 #
@@ -63,28 +66,125 @@ class message_header:
         if self.subject == "" or self.subject == None: # if there is no subject, change the subject line
             self.subject = "[No Subject]"
         # Print in the above format
-        print(f"Message ID:    {self.message_id}\nFrom:          {self.sender}\nSubject:       {self.subject}\nOn:            {self.date}\n\n{self.message}")
+        print((
+            "Message ID:    {}\n"
+            "From:          {}\n"
+            "Subject:       {}\n"
+            "On:            {}\n"
+                             "\n"
+            "{}"
+        ).format(self.message_id, self.sender, self.subject, self.date, self.message))
 
-def parse_input():
-    const help_resp = 
-                    """
-                    -----------------------------------------------------------------------Commands--------------------------------------------------------------------------------
-                    
-                    help                                 : Opens the commands section
-                    connect [address] [port]             : Connects to the running bulletin board server using the address and port.
-                    join                                 : Join the default message board.
-                    post [subject] [message]             : Posts a message with a subject to the default board.
-                    users                                : Retrieves a list of users in the default group.
-                    leave                                : Leave the default group.
-                    message [id]                         : Retrieve the content of the message given by the id.
-                    exit                                 : Exits the client program.
-                    groups                               : Retrieve a list of all groups that can be joined.
-                    groupjoin [name]                     : Join a specific group with given name.
-                    grouppost [name] [subject] [message] : Posts a message with a subject to the board given by the name.
-                    groupusers [name]                    : Retrieves a list of users in the group given by the name.
-                    groupleave [name]                    : Leaves a specific group.
-                    groupmessage [name] [id]             : Retrieves the content of a message using it's id posted earlier on a message board given by its name in a specific group.
+# Class: Invalid Keyword, inherits Exception class
+# Purpose: Raise an exception for a bad keyword
+class InvalidKeyWord(Exception):
+    pass
 
-                    """
+# Class: Invalid Arguments, inherits Exception class
+# Purpose: Raise an exception for incorrect number of arguments
+class InvalidArguments(Exception):
+    pass
 
-    command = input()
+# Class: User
+# Purpose: Store user data, like current port, address, etc. for use later
+#
+# Inputs: username
+
+class user(username: str):
+
+    HELP_TABLE =        """
+                        -----------------------------------------------------------------------Commands--------------------------------------------------------------------------------
+                        
+                        help                                 : Opens the commands section
+                        connect [address] [port]             : Connects to the running bulletin board server using the address and port.
+                        join                                 : Join the default message board.
+                        post [subject] [message]             : Posts a message with a subject to the default board.
+                        users                                : Retrieves a list of users in the default group.
+                        leave                                : Leave the default group.
+                        message [id]                         : Retrieve the content of the message given by the id.
+                        exit                                 : Exits the client program.
+                        groups                               : Retrieve a list of all groups that can be joined.
+                        groupjoin [name]                     : Join a specific group with given name.
+                        grouppost [name] [subject] [message] : Posts a message with a subject to the board given by the name.
+                        groupusers [name]                    : Retrieves a list of users in the group given by the name.
+                        groupleave [name]                    : Leaves a specific group.
+                        groupmessage [name] [id]             : Retrieves the content of a message using it's id posted earlier on a message board given by its name in a specific group.
+
+                        """
+
+    # Function: Parse input
+    # Purpose: take inputs and follow actions like in a command line
+
+    def parse_input():
+        
+
+        command = input()
+
+        keyword = command.split(' ')[0]
+        args = command.split(' ')[1:]
+        
+        if debug_flag:
+            print("Keyword": + keyword)
+            print("Arguments": + args)
+
+        try:
+            match keyword:
+                case "help":
+
+                    break
+                case "connect":
+
+                    break
+        
+                case "join":
+
+                    break
+
+                case "post":
+
+                    break
+
+                case "users":
+
+                    break
+
+                case "leave":
+
+                    break
+
+                case "message":
+
+                    break
+
+                case "exit"
+
+                    break
+
+                case "groups":
+
+                    break
+
+                case "groupjoin":
+
+                    break
+
+                case "grouppost":
+
+                    break
+
+                case "groupusers":
+
+                    break
+
+                case "groupleave":
+
+                    break
+
+                case "groupmessage":
+
+                    break
+
+                case _:
+                    raise(InvalidKeyword())
+        
+        except InvalidKeyword:
